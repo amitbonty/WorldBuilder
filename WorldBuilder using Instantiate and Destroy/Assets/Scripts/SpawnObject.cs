58b7c63019da5f02eb7c497695067c12046b37a3
Amit <amitbontadka@gmail.com>
2021-08-30 18:25:05 +0530

    Implementation #2 using Instantiate and Destroy

---
+++ WorldBuilder using Instantiate and Destroy/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject _prefabToBeSpawnned;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Spawn();
+        }
+    }
+    private void Spawn()
+    {
+        Instantiate(_prefabToBeSpawnned, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
